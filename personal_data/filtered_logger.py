8cb759015b141358d9e60b067e792947f58e2148
LDoualito <3242@holbertonschool.com>
2023-06-02 17:42:49 +0200

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
-"""
-filtered_logger module
-
-This module provides a function to obfuscate specified fields in a log message.
-"""
-
-import re
+import logging
 from typing import List
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -25,3 +34,9 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     pattern = fr'({"|".join(map(re.escape, fields))})=[^{re.escape(separator)}]+(?={re.escape(separator)}|\Z)'
     return re.sub(pattern, fr'\1={redaction}', message)
+
+
+message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+formatter = RedactingFormatter(fields=["email", "ssn", "password"])
+print(formatter.format(log_record))
