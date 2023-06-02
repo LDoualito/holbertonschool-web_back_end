7a2e734e27da39f36fb0dfc0918ac692d9c76e98
LDoualito <3242@holbertonschool.com>
2023-06-02 17:13:17 +0200

    read me + task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates specified fields in the log message.
+
+    Args:
+        fields (list): List of strings representing fields to obfuscate.
+        redaction (str): String used for obfuscation.
+        message (str): Log message.
+        separator (str): Character separating fields in the log message.
+
+    Returns:
+        str: Obfuscated log message.
+    """
+    regex = r'({}=[^{}{}]+)'.format('|'.join(fields), separator, separator)
+    return re.sub(regex, r'\1{}{}'.format(separator, redaction), message)
