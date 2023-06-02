252dfa4d56dd7693cbf3045fe19195461929eacb
LDoualito <3242@holbertonschool.com>
2023-06-02 17:21:58 +0200

    task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ def filter_datum(fields, redaction, message, separator):
     Obfuscates specified fields in the log message.
 
     Args:
-        fields (list): List of strings representing fields to obfuscate.
+        fields (list[str]): List of strings representing fields to obfuscate.
         redaction (str): String used for obfuscation.
         message (str): Log message.
         separator (str): Character separating fields in the log message.
