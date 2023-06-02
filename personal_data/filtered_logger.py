0890b23749d054bccff6ca9ff3823028e7305ec0
LDoualito <3242@holbertonschool.com>
2023-06-02 17:48:02 +0200

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,37 +4,26 @@ import logging
 import re
 from typing import List
 
+
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """ Redacting Formatter class """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super().__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super().format(record)
+        record.msg = self.filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
 
-
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """
-    Obfuscates specified fields in the log message.
-
-    Args:
-        fields (List[str]): List of strings representing fields to obfuscate.
-        redaction (str): String used for obfuscation.
-        message (str): Log message.
-        separator (str): Character separating fields in the log message.
-
-    Returns:
-        str: Obfuscated log message.
-    """
-    pattern = fr'({"|".join(map(re.escape, fields))})=[^{re.escape(separator)}]+(?={re.escape(separator)}|\Z)'
-    return re.sub(pattern, fr'\1={redaction}', message)
+    @staticmethod
+    def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+        pattern = fr'({"|".join(map(re.escape, fields))})=[^{re.escape(separator)}]+(?={re.escape(separator)}|\Z)'
+        return re.sub(pattern, fr'\1={redaction}', message)
 
 
 message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
