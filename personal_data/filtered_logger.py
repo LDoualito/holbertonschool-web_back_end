c12ae6795a4315413cc021b1c5f6a6dd54756625
LDoualito <3242@holbertonschool.com>
2023-06-02 17:56:31 +0200

    task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,61 +1,40 @@
 #!/usr/bin/env python3
 
 import logging
-import re
 from typing import List
 
+PII_FIELDS: List[str] = ['name', 'email', 'phone', 'ssn', 'password']
 
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class.
-
-    This class extends the logging.Formatter class and provides a redacting formatter
-    for log records. It allows filtering values in log records based on specified fields.
-
-    Attributes:
-        REDACTION (str): The string used for redacting field values.
-        FORMAT (str): The log message format.
-        SEPARATOR (str): The character used to separate fields in the log message.
-
-    Args:
-        fields (List[str]): The list of fields to be filtered in log records.
-
-    """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    """ Redacting Formatter class """
 
     def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-        self.fields = fields
+        super().__init__("[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s")
+        self.fields = list(fields)
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format the log record with redacted field values.
+        return filter_datum(self.fields, '***', super().format(record), ';')
 
-        Args:
-            record (logging.LogRecord): The log record to be formatted.
 
-        Returns:
-            str: The formatted log message with redacted field values.
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ Returns the log message obfuscated """
 
-        """
-        record.msg = self.filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
-        return super(RedactingFormatter, self).format(record)
+    for field in fields:
+        message = re.sub(fr'(?<=^|{re.escape(separator)})({"|".join(map(re.escape, [field]))})=[^{re.escape(separator)}]*', fr'\1={redaction}', message)
+    return message
 
-    @staticmethod
-    def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-        """Filter field values in the log message and replace them with the redaction string.
 
-        Args:
-            fields (List[str]): The list of fields to be filtered.
-            redaction (str): The string used to replace the field values.
-            message (str): The log message.
-            separator (str): The character used to separate fields in the log message.
+def get_logger() -> logging.Logger:
+    """ Returns a Logger object named "user_data" """
 
-        Returns:
-            str: The log message with filtered field values.
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
-        """
-        pattern = fr'({"|".join(map(re.escape, fields))})=[^{re.escape(separator)}]+(?={re.escape(separator)}|\Z)'
-        return re.sub(pattern, fr'\1={redaction}', message)
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
