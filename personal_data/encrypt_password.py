08d7fcc106fb4d416cf6b77765dd4956f730f225
LDoualito <3242@holbertonschool.com>
2023-06-02 18:16:48 +0200

    task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
