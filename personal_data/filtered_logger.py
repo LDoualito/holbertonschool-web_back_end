0a5f63f5990adb2bbc5b5dd11c4e07ed25fc0921
LDoualito <3242@holbertonschool.com>
2023-06-02 18:06:42 +0200

    task 3

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,21 +5,14 @@ filtered_logger module
 
 This module provides functions and classes related to logging and database connections.
 """
-
+import logging
 import os
 import mysql.connector
-from mysql.connector.connection import MySQLConnection
 
-
-def get_db() -> MySQLConnection:
-    username: str = os.environ.get('PERSONAL_DATA_DB_USERNAME', 'root')
-    password: str = os.environ.get('PERSONAL_DATA_DB_PASSWORD', '')
-    host: str = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
-    database: str = os.environ.get('PERSONAL_DATA_DB_NAME')
-
-    return mysql.connector.connect(
-        host=host,
-        user=username,
-        password=password,
-        database=database
-    )
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Returns a connector to the database """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(user=username, password=password, host=host, database=db_name)
