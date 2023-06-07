d81c3f7a329527d6ce1becc3e818df982734e980
LDoualito <3242@holbertonschool.com>
2023-06-07 18:11:51 +0200

    task 0 & Readme

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -10,9 +10,9 @@ Base = declarative_base()
 
 class User(Base):
     """User"""
-    __tablename__ ='users'
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
