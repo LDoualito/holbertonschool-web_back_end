c960b6fa02e928c832da02efe09d4d2dd7778056
LDoualito <3242@holbertonschool.com>
2023-06-07 18:31:54 +0200

    task 5 register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -11,3 +11,19 @@ import uuid
 def _hash_password(password: str) -> bytes:
     """ return salted hash of the input password """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ save the user to the database _db and return a User """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
