2a9002def08ff7bcd27e0ff35e94436266418efe
LDoualito <3242@holbertonschool.com>
2023-07-09 15:42:19 +0200

    task 100

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -2,6 +2,6 @@ export default function createIteratorObject(report) {
   const employees = [];
   for (const department of Object.keys(report.allEmployees)) {
     employees.push(...report.allEmployees[department]);
-    }
+  }
   return employees;
 }
