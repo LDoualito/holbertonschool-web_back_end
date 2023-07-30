cb966afc6d2e81440f5c8a398cf26aa4a96a589c
LDoualito <3242@holbertonschool.com>
2023-07-30 19:40:07 +0200

    correct syntax task 7

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+    if (typeof set !== 'object') return '';
+    if (typeof startString !== 'string') return '';
+    if (startString.length === 0) return '';
+  
+    const list = [];
+    set.forEach((e) => {
+      if (e && e.startsWith(startString)) list.push(e.replace(startString, ''));
+      return e;
+    });
+    return list.join('-');
+  }
