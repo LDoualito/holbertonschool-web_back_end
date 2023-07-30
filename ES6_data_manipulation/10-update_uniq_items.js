42372956e57693a30ce3ccd01f3a29e0e80ef78f
LDoualito <3242@holbertonschool.com>
2023-07-30 19:45:31 +0200

    task 10 passes int

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,8 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-    map.forEach((v, k) => {
-      if (v === 1) map.set(k, 100);
-    });
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
+  map.forEach((v, k) => {
+    if (v === 1) map.set(k, 100);
+  });
+}
