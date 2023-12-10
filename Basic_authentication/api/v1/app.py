2a95c14e028bccc92ceddc279f81fe9539a0e42a
LDoualito <3242@holbertonschool.com>
2023-12-10 21:41:56 +0100

    documentation

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -23,25 +23,33 @@ else:
 
 @app.errorhandler(404)
 def not_found(error):
-    """ Not found handler """
+    """
+    Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error):
-    """ Unauthorized handler """
+    """
+    Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error):
-    """ Forbidden handler """
+    """
+    Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before_request():
-    """ Execute before request """
+    """
+    Execute before request
+    """
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
 
     if auth is None:
@@ -55,7 +63,9 @@ def before_request():
 
 
 if __name__ == "__main__":
-    """ Main entry point of the application """
+    """
+    Main entry point of the application
+    """
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
